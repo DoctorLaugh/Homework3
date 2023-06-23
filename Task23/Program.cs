d4e1724fdc0c2694074e83be5deb487ef4a9d37a
Dmitriy <trayan@yandex.ru>
2023-06-23 12:30:16 +1100

    Task21 added

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите натуральное число");
+int num = int.Parse(Console.ReadLine());
+if (num <= 0)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+int[] result = new int[num];
+for(int i=1; i<=num; i++)
+result[i-1] = Convert.ToInt32(Math.Pow(i, 3));
+for(int i=0; i<num; i++)
+Console.Write($"{result[i]} ");
