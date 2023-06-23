4b831b83384fd279ecdab29cfd632c67dad064a2
Dmitriy <trayan@yandex.ru>
2023-06-23 12:06:54 +1100

    Task19 added

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+if (num<10000 || num>99999)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+int num5 = num%10;
+int num4 = (num/10)%10;
+int num2 = (num/1000)%10;
+int num1 = num/10000;
+if(num5==num1 && num4==num2)
+{
+    Console.WriteLine("Число - палиндром");
+}
+else
+{
+    Console.WriteLine("Число - не палиндром");
+}
